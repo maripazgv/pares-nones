207f313c7e5dfe7a6f9b4c3f82ebe75564c5bd95
maripazgv <maripazgv.creacionweb@gmail.com>
2017-09-28 14:57:05 +0200

    juego pares o nones

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("no").onclick=function(){
+    document.getElementById("quieresjugar").style.display="none";
+    document.getElementById("nojuego").style.display="block";
+}
+document.getElementById("si").onclick=function(){
+    
+}
